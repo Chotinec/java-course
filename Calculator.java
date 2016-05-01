e9a24190105515ed99d7ec39ca7f327f6eaf7cdf
Artiom Hotinec <artiom-hotinec@rambler.ru>
2016-05-02 00:02:09 +0300

    lesson 4 home work

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,12 @@
+import java.lang.Math;
+
 public class Calculator{
        
         /**
                 Результат Вычислений
         */
-        private int result;
-
-        /**
-                Сумируем аргументы
-                @param params аргументы суммирования
-        */
-        public void add(double ... params){
-              for (Double param : params){
-                this.result += param;
-              }  
-        }
+        private double result;
+        private boolean oldResult = false;
 
         /**
                 Получить результат
@@ -29,4 +22,65 @@ public class Calculator{
         public void cleanResult(){
                 this.result = 0;
         }
+
+        /**
+                Использовать/не использовать старый результат
+        */
+        public void setOldResult(boolean res){
+                oldResult = res;
+        }
+
+        /**
+                Сумируем аргументы
+                @param params аргументы суммирования
+        */
+        public void add(double param1, double param2){
+              if (oldResult)
+                this.result = this.result + param1 + param2;
+              else
+                this.result = param1 + param2;
+        }
+
+        /**
+                Вычитаем аргументы
+        */
+        public void subtract(double param1, double param2){
+              if (oldResult)
+                this.result = this.result - param1 - param2;
+              else
+                this.result = param1 - param2;
+        }
+
+        /**
+                Умножаем аргументы
+        */
+        public void multiplication(double param1, double param2){
+              if (oldResult)
+                this.result = this.result * param1 * param2;
+              else
+                this.result = param1 * param2;
+        }
+
+        /**
+                Деление аргументов
+        */
+        public void division(double param1, double param2){
+              if (oldResult)
+                this.result = this.result / param1 / param2;
+              else
+                this.result = param1 / param2;
+        }
+
+        /**
+                Извлечение корня
+        */
+        public void involution(double param1, double param2){
+              if (oldResult){
+                this.result = Math.pow(Math.pow(this.result, param1),param2);
+              } else {
+                this.result = Math.pow(param1, param2);
+              }
+        }
+
+
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -10,16 +10,47 @@ public class InteractRunner{
                 try{
                         Calculator calc  = new Calculator();
                         String exit = "no";
+                        boolean oldResult = false;
                         while(!exit.equals("yes")){
+                                System.out.println("Choose operation: \n1 - add operation \n2 - subtract operation \n3 - multiplication operation \n4 - division operation \n5 - involution operation");
+                                String operation = reader.next();
                                 System.out.println("Enter first arg : ");
                                 String first = reader.next();
                                 System.out.println("Enter second arg : ");
                                 String second = reader.next();
-                                calc.add(Double.valueOf(first),Double.valueOf(second));
+                                calc.setOldResult(oldResult);
+                                switch(Integer.valueOf(operation)){
+                                        case 1:
+                                           calc.add(Double.valueOf(first),Double.valueOf(second));
+                                           break;
+                                        case 2:
+                                           calc.subtract(Double.valueOf(first),Double.valueOf(second));
+                                           break;
+                                        case 3:
+                                           calc.multiplication(Double.valueOf(first),Double.valueOf(second)); 
+                                           break;
+                                        case 4:
+                                           calc.division(Double.valueOf(first),Double.valueOf(second)); 
+                                           break;
+                                        case 5:
+                                           calc.involution(Double.valueOf(first),Double.valueOf(second)); 
+                                           break;
+                                        default: 
+                                           System.out.println("Incorrect input, try again!");
+                                           continue;                 
+                                }
                                 System.out.println("Result is " + calc.getResult());
-                                calc.cleanResult();
+                                //calc.cleanResult();
                                 System.out.println("Exit : yes/no");
                                 exit = reader.next();
+                                 if (!exit.equals("yes")){
+                                        System.out.println("Do you whant to use old result : yes/no");
+                                        String old = reader.next();
+                                        if ("yes".equals(old))
+                                                oldResult = true;
+                                        else
+                                                oldResult = false;      
+                                }
                         }
                 } finally{
                         reader.close();
